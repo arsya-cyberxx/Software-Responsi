9f58b17927ac481097ba3acfbbc5960ec45e3d6b
Matahari Arsya <159606195+arsya-cyberxx@users.noreply.github.com>
2024-11-12 06:11:07 +0000

    m

--- Payment/Ngambil Cart.py
+++ Payment/Ngambil Cart.py
@@ -12,21 +12,38 @@ def comserial(com_port, baud_rate=9600):
                         timeout=3)
     return ser
 
-def send_stock(file_path, ser):
+def send_stock(file_path):
     df = pd.read_csv(file_path)
     last_value_cart = df['cart'][df['cart'].notna()].iloc[-1] 
-    ser.write(last_value_cart.encode())
+    last_value_cart=last_value_cart.split(",")
+    totalharga=0
+    for i in range(0, len(last_value_cart), 2):
+        if last_value_cart[i]=='A':
+            totalharga+=1*int(last_value_cart[i+1])
+        elif last_value_cart[i]=='B':
+            totalharga+=2*int(last_value_cart[i+1])
+        elif last_value_cart[i]=='C':
+            totalharga+=3*int(last_value_cart[i+1])
+        elif last_value_cart[i]=='D':
+            totalharga+=4*int(last_value_cart[i+1])
+    #ser.write(last_value_cart.encode())
+    totalharga=str(totalharga)
+    if len(totalharga)!=3:
+        totalharga='0'*(3-len(totalharga))+totalharga
+    
+    binerharga=[]
+    for i in totalharga:
+        biner = format(int(i), '004b')
+        binerharga.append(biner)
+    return binerharga
 
 if __name__ == "__main__":
     file_path = 'okedeh.csv'
     com_port = 'COM3'
     baud_rate = 9600
 
-    ser = comserial(com_port, baud_rate)
     
     try:
-        send_stock(file_path, ser)  
+        print(send_stock(file_path)) 
     except KeyboardInterrupt:
         print("Process interrupted. Closing COM port.")
-    finally:
-        ser.close()
--- Payment/Update CSV.py
+++ Payment/Update CSV.py
@@ -1,34 +1,37 @@
 import serial
 import csv
+import pandas as pd
 
-com = serial.Serial(port='COM4',
-                    baudrate=19200,
-                    parity=serial.PARITY_EVEN,
-                    stopbits=serial.STOPBITS_ONE,
-                    bytesize=serial.EIGHTBITS,
-                    timeout=3)
+# com = serial.Serial(port='COM3',
+#                     baudrate=19200,
+#                     parity=serial.PARITY_EVEN,
+#                     stopbits=serial.STOPBITS_ONE,
+#                     bytesize=serial.EIGHTBITS,
+#                     timeout=3)
 
-com_bytes = com.readline()
-data = com_bytes.decode("unicode").strip()
+# com_bytes = com.readline()
+# data = com_bytes.decode("unicode").strip()
 
-file_path = 'okedeh.csv'
+file_path = 'okedehh.csv'
+df=pd.read_csv(file_path)
+last_value_cart = df['cart'][df['cart'].notna()].iloc[-1] 
 
 with open(file_path, mode='r+', newline='') as file:
     reader = csv.DictReader(file)
     rows = []
 
-    cart_items = data.split(',')
+    cart_items = last_value_cart.split(',')
 
     for row in reader:
-        stock_name = row['stock']
+        stock_name = row['product_ID']
 
         if stock_name in cart_items:
             index = cart_items.index(stock_name)
             quantity_to_deduct = int(cart_items[index + 1])
             
-            current_quantity = int(row['quantity'])
+            current_quantity = int(row['product_stock']) if row['product_stock'] else 0
             updated_quantity = max(current_quantity - quantity_to_deduct, 0)
-            row['quantity'] = str(updated_quantity)
+            row['product_stock'] = int(updated_quantity)
         
         rows.append(row)
 
