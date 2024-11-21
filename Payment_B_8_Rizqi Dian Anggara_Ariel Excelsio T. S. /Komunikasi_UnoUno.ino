f375146675313c124d850eb9416325d7dd6ba7b3
Matahari Arsya <159606195+arsya-cyberxx@users.noreply.github.com>
2024-11-21 14:20:40 +0700

    Update Komunikasi_UnoUno.ino

--- Payment_B_8_Rizqi Dian Anggara_Ariel Excelsio T. S. /Komunikasi_UnoUno.ino
+++ Payment_B_8_Rizqi Dian Anggara_Ariel Excelsio T. S. /Komunikasi_UnoUno.ino
@@ -1,35 +1,27 @@
 #include <RH_ASK.h>
-#include <SoftwareSerial.h>
-
-SoftwareSerial psoc(2,3);  // Define Pin serial UART antara PSoC dan Arduino Receiver
+#include <SoftwareSerial.h> 
 RH_ASK driver;  // RF driver (pin 12 TX, pin 11 RX)
 
 void setup() {
-  Serial.begin(9600);  // Inisiasi UART antar Arduino
-  psoc.begin(9600); // Inisiasi UART antar Arduino receiver dan PSoC
+    Serial.begin(9600);  // Inisiasi Serial untuk komunikasi UART
 }
 
 void loop() {
-  uint8_t buf[100]; // Buffer untuk menerima data
-  uint8_t buflen = sizeof(buf);       // Size dari buffer
+    // Menerima data dari phyton berupa total harga
+    if (uno.available() > 0) {
+        String data = uno.readString(); 
+        uno.print(data); // Membaca data total harga via komunikasi serial
 
-  // Memeriksa ukuran data yang diterim
-  if (Serial.available() > 0) {
-      String receivedData = Serial.readString(); // Membaca data sampai newline
-      Serial.println(receivedData); // Menampilkan data di Serial Monitor
-      psoc.write(receivedData.c_str()); // Mengirim data ke PSoC
-  }
+        // Mengirim data melalui RF
+        const char msg = data.c_str();  // Konversi String to char
+        driver.send((uint8_t *)msg, strlen(msg));  // Mengirim data
+        driver.waitPacketSent();  
+    }
 
-  // Menerima data komparasi dari PSoC ke arduino receiver dengan UART
-  if (psoc.available()>0) {
-    String data_succes = "Payment Success"; // String penanda payment berhasil
-    String data_gagal = "Payment Failed"; // String penanda payment gagal
-    String data_psoc = psoc.readString();
-    if (data_psoc == data_succes){
-      Serial.println("Sukses"); // String untuk mengupdate stock
+    // Menerima hasil komparasi input user dan total harga dari user via uart
+    if (Serial.available()>0){
+      String uno = Serial.readString(); 
+      Serial.print(uno); // Mengirim data untuk update data server
     }
-    if (data_psoc == data_gagal) {
-      Serial.println("Gagal"); //String untuk mengupdate login status dan mengeksekusi fungsi security
-    }
-  }
+    delay(1000);  // Delay for 1 second before checking again
 }
